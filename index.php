e50226ad41fdf54493d1e712f993382ce8045e63
saidmoussadek <saiddus24@gmail.com>
2022-09-14 09:16:15 +0200

    push

--- index.php
+++ index.php
@@ -65,26 +65,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
 </head>
     <body>
-       <?php
+       
+       <header>
+       </header>
 
-            foreach($Faqs as $Faq){
-                // var_dump($Faq['domanda']);
-                echo "<h2 class='titolo'>". $Faq['domanda'] ."</h2>"; 
-                echo '<br>';
-                echo "<p class='text'>" .$Faq['risposta']."</p>";
-                echo '<br>'; 
-
-                    
-                }
-
-
-        ?>
-     
-
-        
+       <main>
+        <?php
+                foreach($Faqs as $Faq){
+                    // var_dump($Faq['domanda']);
+                    echo "<h2 class='titolo'>". $Faq['domanda'] ."</h2>"; 
+                    echo '<br>';
+                    echo "<p class='text'>" .$Faq['risposta']."</p>";
+                    echo '<br>';    
+                    }
+            ?>
+       </main> 
+       
+       
+       <footer>      
+       
+       </footer>
+      
     </body>
 </html>
